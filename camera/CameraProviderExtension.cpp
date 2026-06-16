914555cdd3e6fecadf774c4b9f2f14813210bd60
lemezohaib <zhbkhan644@gmail.com>
2026-06-16 19:22:41 +0500

    fixup! davinci: camera: Cap max_brightness
    *255 is way too high for davinci and could cause led damage
    
    Change-Id: Id9315cc29b28efc57f70a9de0872b4d16b549c14
    Signed-off-by: lemezohaib <zhbkhan644@gmail.com>

--- camera/CameraProviderExtension.cpp
+++ camera/CameraProviderExtension.cpp
@@ -47,15 +47,11 @@ bool supportsSetTorchModeExt() {
 }
 
 int32_t getTorchDefaultStrengthLevelExt() {
-    // Our default value is 75. This corresponds to 15%.
-    // As we have changed the maximum value, 59% now corresponds to 75.
-    return 59;
+    return 50;
 }
 
 int32_t getTorchMaxStrengthLevelExt() {
-    // 255 out of 500 is a sane brightness.
-    // Let's cap it to 255 as max, we can go much higher, but I don't want to test this.
-    return 255;
+    return 75;
 }
 
 int32_t getTorchStrengthLevelExt() {
